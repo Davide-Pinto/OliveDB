e5e8417b80adcf711aff7b3380f7d1f2c10595ea
Davide-Pinto <114738638+Davide-Pinto@users.noreply.github.com>
2024-06-30 11:21:00 +0100

    update

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Test
+from .models import Client
 
-# Register your models here.
-admin.site.register(Test)
+admin.site.register(Client)
+
 
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(requenst):
+    return render(requenst, 'home.html', {})
